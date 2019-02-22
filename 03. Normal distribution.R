885b3d0d0b3c3250499530765ce9d3d2408177b4
Jongjin Kim <jkim205@crimson.ua.edu>
2019-02-22 13:07:15 -0600

    Normal distribution

--- 03. Normal distribution.R
+++ 03. Normal distribution.R
@@ -1,4 +1,4 @@
-# Chapter 6 Normal Distribution
+# Chapter 6 Normal Distribution and Chapter 7 Probability and Sampling distributions
 
 # plot the normal distribution
 sd <- c(-3, -2, -1, 0, 1, 2, 3)
@@ -14,6 +14,25 @@ plot(sd, density, type = 'l')
 pnorm(2) - pnorm(-2)      # it turns out around 95%
 
 # make a random sample with a size of 1000 from normal distribution with mean = 50 and sd = 10
-data <- rnorm(n=1000, mean = 50, sd = 10)
-zscore <- (data - 50) / 10
-zscore
+mu <- 50
+sigma <- 10
+data <- rnorm(n=1000, mean = mu, sd = sigma)      # generate 1000 samples on the normal distribution
+zscore <- (data - mu) / sigma                     # calculate z score
+head(cbind(data,zscore))                          # show first 6 entries of data and z score
+
+# histogram of 1000 samples
+hist(data, breaks=30)
+hist(zscore, breaks=30)
+
+# table 6.2 in p185.
+
+sat_data <- c(500, 750, 600, 900, 950, 880, 990, 560, 780, 800, 800, 450, 800, 680, 550, 600)
+
+length(sat_data)     # sample size
+range(sat_data)      # min and max of the sample
+mu <- mean(sat_data)       # sample mean
+summary(sat_data)    # or use a summary function
+sigma <- sd(sat_data)         # standard deviation
+
+sat_zscore <- (sat_data - mu) / sigma                     # calculate z score
+cbind(sat_data, sat_zscore)
