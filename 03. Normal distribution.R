d48740aee34882166b613b570ad8c9affdc4924a
Jongjin Kim <jkim205@crimson.ua.edu>
2019-02-22 01:16:53 -0600

    normal distribution R code

--- 03. Normal distribution.R
+++ 03. Normal distribution.R
@@ -9,3 +9,11 @@ plot(sd, density, type = 'o')
 sd <- seq(-3, 3, length.out = 100)
 density <- dnorm(sd)
 plot(sd, density, type = 'l')
+
+# the probability between -2SD and 2SD
+pnorm(2) - pnorm(-2)      # it turns out around 95%
+
+# make a random sample with a size of 1000 from normal distribution with mean = 50 and sd = 10
+data <- rnorm(n=1000, mean = 50, sd = 10)
+zscore <- (data - 50) / 10
+zscore
