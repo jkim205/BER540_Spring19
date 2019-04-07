310d10e0eb91cbd1eaa10839262139c8accb5bc2
Jongjin Kim <jkim205@crimson.ua.edu>
2019-04-06 23:55:51 -0500

    delete data frame

--- 08. linear regression.R
+++ 08. linear regression.R
@@ -10,19 +10,17 @@ scatter.smooth(x,y)
 model <- lm(y~x)
 summary(model)
 
+
 # multiple regression Example 16.4 
 age <- c(19, 21, 26, 28, 32, 30)
 education <- c(12, 14, 13, 18, 17, 16)
 sales <- c(20, 40, 30, 68, 70, 60)
 
-df <- data.frame(age, education, sales)
-
-model <- lm(sales~ age + education, data = df)
+model <- lm(sales~ age + education)
 summary(model)
 
+# standardized coefficients
 b <- summary(model)$coef[2:3,1]
-
 sy <- apply(model$model[1], 2, sd)
 sx <- apply(model$model[2:3], 2, sd)
-
 betas <- b * (sx/sy)
